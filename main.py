75ca34a8462c2d7ec2fb8b2d83afb49f104ccfcf
gbach415 <g.bachmann1@gmail.com>
2021-11-15 10:36:23 -0500

    add getMessages

--- main.py
+++ main.py
@@ -58,24 +58,65 @@ def getTopTenLikes(period, groupID):
 
     return df[['name', 'likes']]
 
-def getMessages(groupID):
+def getMessages(groupID, lastID=None):
     PARAMS = {"token": ACCESS_TOKEN,
-              "limit": 100}
+              "limit": 100,
+              "before_id": lastID}
 
     requestURL = URL + f"groups/{groupID}/messages"
 
     response = requests.get(url=requestURL, params=PARAMS)
 
     data = json.dumps(response.json(), indent=3)
-    # print(data)
 
     data = json.loads(data)
 
     df = pd.DataFrame.from_dict(data['response']['messages'])
 
     df = df[['name', 'user_id', 'id']]
+
     lastID = df['id'].iloc[-1]
-    
+
+    while response.status_code == 200:
+        PARAMS = {"token": ACCESS_TOKEN,
+                  "limit": 100,
+                  "before_id": lastID}
+        response = requests.get(url=requestURL, params=PARAMS)
+        if response.status_code != 200: break
+
+        tempData = json.dumps(response.json(), indent=3)
+
+        tempData = json.loads(tempData)
+
+        tempDF = pd.DataFrame.from_dict(tempData['response']['messages'])
+
+        tempDF = tempDF[['name', 'user_id', 'id']]
+        lastID = tempDF['id'].iloc[-1]
+
+        df = df.append(tempDF, ignore_index=True)
+
+    # uniqueUserIDs = list(df['user_id'].unique())
+
+    """
+    Make note that if going by name instead of user_id, it won't include all messages for a person if their name has changed
+    """
+    totalCount = df['user_id'].value_counts()
+    # totalCount = df['name'].value_counts()
+
+    finalDF = pd.DataFrame(data=totalCount).reset_index()
+    finalDF = finalDF.rename(columns={"index": "user_id", "user_id": "count"})
+    finalDF['name'] = None
+
+    user_id_mapping = dict(zip(df.user_id, df.name))
+
+    for i in range(len(finalDF) - 1):
+        if finalDF['name'][i] == None:
+            name = user_id_mapping[finalDF['user_id'][i]]
+            # print(f"user_id: {finalDF['user_id'][i]} -- name: {name}")
+            finalDF['name'][i] = name
+
+    print(finalDF)
+
 
 
 def main():
